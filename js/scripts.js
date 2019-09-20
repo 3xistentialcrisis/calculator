16fa1fd35195c1005ce78efc478b8a44d6ac9b15
Wanjiku Karanja <njahirakaranja@Njahiras-MacBook-Air.local>
2019-09-20 15:16:48 +0300

    Create index.html webpage

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var divide = function(number1, number2){
+return number1 / number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+alert(number1)
+
+var number2 = parseInt(prompt("Enter another number:"));
+alert(divide(number1,number2));
